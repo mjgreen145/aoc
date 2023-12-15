d9bf89e7a0ed40e4e75aba5ea3f69f2eb936153e
mjgreen145 <mjgreen145@gmail.com>
2023-12-15 13:42:58 +0000

    Day 15 refactor

--- src/main/kotlin/Day15.kt
+++ src/main/kotlin/Day15.kt
@@ -12,26 +12,25 @@ fun main() {
         return input.split(",").map(::hash).sum()
     }
 
-    fun part2(input: String): Int {
+    fun part2(input: String): Long {
         val regex = Regex("([a-zA-Z]+)([-=])(\\d?)")
-        val boxes = (0..255).associateWith { linkedMapOf<String, Int>() }
+        val boxes = Array(256) { linkedMapOf<String, Int>() }
 
         input.split(",").forEach {
             val (_, label, op) = regex.find(it)!!.groupValues
             val box = hash(label)
             when (op) {
-                "-" -> boxes[box]!!.remove(label)
-                "=" -> boxes[box]!![label] = regex.find(it)!!.groupValues[3].toInt()
+                "-" -> boxes[box].remove(label)
+                "=" -> boxes[box][label] = regex.find(it)!!.groupValues[3].toInt()
                 else -> throw Exception("Unsupported op, '$op'")
             }
         }
 
-        return ((0..255).sumOf { i ->
-            val values = boxes[i]!!.values
-            val result: Int = if (values.isEmpty()) 0 else
-                values.foldIndexed(0) { j, total, lens -> total + (lens * (j + 1) * (i + 1)) }
-            result
-        })
+        return boxes.withIndex().sumOf { (i, box) ->
+            box.values.foldIndexed(0) { j, total, lens ->
+                total + (lens * (j + 1) * (i + 1))
+            }.toLong()
+        }
     }
 
     val part1Example = part1(exampleInput)
@@ -41,7 +40,7 @@ fun main() {
     println("Part 1 took $timePart1")
 
     val part2Example = part2(exampleInput)
-    check(part2Example == 145) { -> "Part 2 example failed: Expected 145, received $part2Example" };
+    check(part2Example == 145L) { -> "Part 2 example failed: Expected 145, received $part2Example" };
 
     val timePart2 = measureTime { part2(input).println() }
     println("Part 2 took $timePart2")
