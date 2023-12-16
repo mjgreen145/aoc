5e36cf0bc7d5e0c358283d20e101abd6234dd012
mjgreen145 <mjgreen145@gmail.com>
2023-12-16 16:21:28 +0000

    Day 5 part 2

--- src/main/kotlin/Day5.kt
+++ src/main/kotlin/Day5.kt
@@ -1,3 +1,5 @@
+import kotlin.math.max
+import kotlin.math.min
 import kotlin.time.measureTime
 
 fun rangeIntersection(a: LongRange, b: LongRange): LongRange? {
@@ -8,27 +10,69 @@ fun rangeIntersection(a: LongRange, b: LongRange): LongRange? {
 }
 
 fun splitBy(a: LongRange, b: LongRange): Pair<LongRange?, List<LongRange>> {
-    val rangeBefore = if (a.first < b.first) LongRange(a.first, b.first - 1) else null
+    val rangeBefore = if (a.first < b.first) LongRange(a.first, min(a.last, b.first - 1)) else null
     val rangeOverlap = rangeIntersection(a, b);
-    val rangeAfter = if (a.last > b.last) LongRange(b.last + 1, a.last) else null
+    val rangeAfter = if (a.last > b.last) LongRange(max(b.last + 1, a.first), a.last) else null
 
     return Pair(rangeOverlap, listOfNotNull(rangeBefore, rangeAfter));
+}
+
+typealias LocationMap = List<Pair<LongRange, Long>>
+
+fun getSeeds(input: String): List<Long> {
+    return input.split("\n\n")
+        .first()
+        .substringAfter(": ")
+        .split(" ")
+        .map { it.toLong() }
+}
+
+fun getSeedRanges(input: String): List<LongRange> {
+    return input.split("\n\n")
+        .first()
+        .substringAfter(": ")
+        .split(" ")
+        .map { it.toLong() }
+        .chunked(2)
+        .map { (rangeStart, rangeLength) -> LongRange(rangeStart, rangeStart + rangeLength - 1) }
+}
+
+fun getLocationMaps(input: String): List<LocationMap> {
+    return input.split("\n\n")
+        .drop(1)
+        .map {
+            it.split("\n").drop(1).map { line ->
+                val (destRangeStart, sourceRangeStart, rangeLength) = line.split(" ").map { num -> num.toLong() }
+                Pair(LongRange(sourceRangeStart, sourceRangeStart + rangeLength - 1), destRangeStart - sourceRangeStart)
+            }
+        }
+}
+
+fun mapOneLocation(locationMap: LocationMap, ranges: List<LongRange>): List<LongRange> {
+    val processedRanges = mutableListOf<LongRange>()
+    var rangesToProcess = ranges.toMutableList()
+
+    for ((mapRange, mapDiff) in locationMap) {
+        rangesToProcess = rangesToProcess.flatMap { range ->
+            val (overlap, others) = splitBy(range, mapRange)
+            if (overlap != null) {
+                processedRanges.add(LongRange(overlap.first + mapDiff, overlap.last + mapDiff))
+            }
+            others
+        }.toMutableList()
+    }
+    processedRanges.addAll(rangesToProcess)
+
+    return processedRanges
 }
 
 fun main() {
     val exampleText = readInput("day5-example")
     val text = readInput("day5")
-    val blocks = exampleText.split("\n\n")
 
-    val maps = blocks.drop(1).map {
-        it.split("\n").drop(1).map { line ->
-            val (destRangeStart, sourceRangeStart, rangeLength) = line.split(" ").map { num -> num.toLong() }
-            Pair(LongRange(sourceRangeStart, sourceRangeStart + rangeLength - 1), destRangeStart - sourceRangeStart)
-        }
-    }
-
-    fun part1(): Long {
-        val seeds = blocks.first().substringAfter(": ").split(" ").map { it.toLong() }
+    fun part1(input: String): Long {
+        val seeds = getSeeds(input);
+        val maps = getLocationMaps(input)
 
         val locations = seeds.map { seed ->
             maps.fold(seed) { value, map ->
@@ -40,35 +84,25 @@ fun main() {
         return locations.min()
     }
 
-    fun part2(): Long {
-        val seedRanges = blocks.first().substringAfter(": ").split(" ").map { it.toLong() }.chunked(2)
-            .map { (rangeStart, rangeLength) -> LongRange(rangeStart, rangeStart + rangeLength - 1) }
+    fun part2(input: String): Long {
+        val seedRanges = getSeedRanges(input)
+        val locationMaps = getLocationMaps(input)
 
-        val finalRanges = maps.foldIndexed(seedRanges) { i, ranges, mapBlock ->
-            println("Doing block $i of ${maps.size}")
-            println(ranges.size)
-            mapBlock.flatMap { map ->
-                ranges.flatMap { range ->
-                    val (overlap, others) = splitBy(range, map.first)
-                    val mappedRange = if (overlap != null) LongRange(
-                        overlap.first + map.second,
-                        overlap.last + map.second
-                    ) else null
-                    val combined: List<LongRange> = if (mappedRange != null) {
-                        val newRanges = others.toMutableList()
-                        newRanges.addLast(mappedRange);
-                        newRanges
-                    } else others
-                    combined
-                }
-            }
+        val finalRanges = locationMaps.fold(seedRanges) { ranges, locationMap ->
+            mapOneLocation(locationMap, ranges)
         }
-
-        return finalRanges.sortedBy { it.first }.first().first
+        return finalRanges.minOf { range -> range.first }
     }
 
-    val timePart1 = measureTime { part1().println() }
+    val part1Example = part1(exampleText)
+    val part2Example = part2(exampleText)
+
+    check(part1Example == 35L) { -> "Part 1 example failed: Expected 35, received $part1Example" };
+    check(part2Example == 46L) { -> "Part 2 example failed: Expected 46, received $part2Example" };
+
+    val timePart1 = measureTime { part1(text).println() }
+    val timePart2 = measureTime { part2(text).println() }
+
     println("Part 1 took $timePart1")
-    val timePart2 = measureTime { part2().println() }
     println("Part 2 took $timePart2")
 }
--- src/main/kotlin/Template.kt
+++ src/main/kotlin/Template.kt
@@ -13,14 +13,14 @@ fun main() {
     }
 
     val part1Example = part1(exampleLines)
-    check(part1Example == 0) { -> "Part 1 example failed: Expected 0, received $part1Example" };
-
-    val timePart1 = measureTime { part1(lines).println() }
-    println("Part 1 took $timePart1")
-
     val part2Example = part2(exampleLines)
+
+    check(part1Example == 0) { -> "Part 1 example failed: Expected 0, received $part1Example" };
     check(part2Example == 0) { -> "Part 2 example failed: Expected 0, received $part2Example" };
 
+    val timePart1 = measureTime { part1(lines).println() }
     val timePart2 = measureTime { part2(lines).println() }
+
+    println("Part 1 took $timePart1")
     println("Part 2 took $timePart2")
 }
