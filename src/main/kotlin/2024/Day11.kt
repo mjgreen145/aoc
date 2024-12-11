0a60c312fc5b3c167a1ee476f13d581dd1bbfcb2
mjgreen145 <mjgreen145@gmail.com>
2024-12-11 08:30:47 +0000

    2024: Day 11

---
+++ src/main/kotlin/2024/Day11.kt
@@ -0,0 +1,58 @@
+package `2024`
+
+import println
+import readInput
+import kotlin.time.measureTime
+
+fun blink(stone: Long): List<Long> {
+    if (stone == 0L) return listOf(1L)
+    val str = stone.toString()
+    if (str.length % 2 == 0) {
+        return listOf(
+            str.substring(0, str.length / 2).toLong(),
+            str.substring(str.length / 2).toLong()
+        )
+    }
+    return listOf((stone * 2024L))
+}
+
+val cache = mutableMapOf<Pair<Long, Int>, Long>()
+
+fun numStonesGenerated(stone: Long, blinksLeft: Int): Long {
+    if (blinksLeft == 0) return 1
+    return blink(stone).sumOf { numStonesGeneratedCached(it, blinksLeft - 1) }
+}
+
+fun numStonesGeneratedCached(stone: Long, blinksLeft: Int): Long {
+    val cached = cache[Pair(stone, blinksLeft)]
+    if (cached != null) return cached
+
+    val ans = numStonesGenerated(stone, blinksLeft)
+    cache[Pair(stone, blinksLeft)] = ans
+    return ans
+}
+
+fun main() {
+    val exampleLines = readInput("2024", "day11-example")
+    val input = readInput("2024", "day11")
+
+    fun part1(input: String): Long {
+        val startStones = input.split(" ").map { it.toLong() }
+        return startStones.sumOf { numStonesGeneratedCached(it, 25) }
+    }
+
+    fun part2(input: String): Long {
+        val startStones = input.split(" ").map { it.toLong() }
+        return startStones.sumOf { numStonesGeneratedCached(it, 75) }
+    }
+
+    val part1Example = part1(exampleLines)
+
+    check(part1Example == 55312L) { -> "Part 1 example failed: Expected 55312, received $part1Example" };
+
+    val timePart1 = measureTime { part1(input).println() }
+    val timePart2 = measureTime { part2(input).println() }
+
+    println("Part 1 took $timePart1")
+    println("Part 2 took $timePart2")
+}
--- src/main/kotlin/Template.kt
+++ src/main/kotlin/Template.kt
@@ -1,10 +1,8 @@
-import println
-import readLines
 import kotlin.time.measureTime
 
 fun main() {
-    val exampleLines = readLines("2024", "day1-example")
-    val lines = readLines("2024", "day1")
+    val exampleInput = readLines("2024", "day1-example")
+    val input = readLines("2024", "day1")
 
     fun part1(lines: List<String>): Int {
         return 0
@@ -14,14 +12,14 @@ fun main() {
         return 0
     }
 
-    val part1Example = part1(exampleLines)
-    val part2Example = part2(exampleLines)
+    val part1Example = part1(exampleInput)
+    val part2Example = part2(exampleInput)
 
     check(part1Example == 0) { -> "Part 1 example failed: Expected 0, received $part1Example" };
     check(part2Example == 0) { -> "Part 2 example failed: Expected 0, received $part2Example" };
 
-    val timePart1 = measureTime { part1(lines).println() }
-    val timePart2 = measureTime { part2(lines).println() }
+    val timePart1 = measureTime { part1(input).println() }
+    val timePart2 = measureTime { part2(input).println() }
 
     println("Part 1 took $timePart1")
     println("Part 2 took $timePart2")
