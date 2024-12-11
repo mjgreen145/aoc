ff342ececcb695dfe65e74ae4aa53c32b1513036
mjgreen145 <mjgreen145@gmail.com>
2024-12-11 09:50:55 +0000

    2024: Day 11 - refactor to use Map.getOrPut

--- src/main/kotlin/2024/Day11.kt
+++ src/main/kotlin/2024/Day11.kt
@@ -4,50 +4,38 @@ import println
 import readInput
 import kotlin.time.measureTime
 
-fun blink(stone: Long): List<Long> {
-    if (stone == 0L) return listOf(1L)
-    val str = stone.toString()
-    if (str.length % 2 == 0) {
-        return listOf(
-            str.substring(0, str.length / 2).toLong(),
-            str.substring(str.length / 2).toLong()
-        )
-    }
-    return listOf((stone * 2024L))
-}
-
-val cache = mutableMapOf<Pair<Long, Int>, Long>()
-
-fun numStonesGenerated(stone: Long, blinksLeft: Int): Long {
-    if (blinksLeft == 0) return 1
-    return blink(stone).sumOf { numStonesGeneratedCached(it, blinksLeft - 1) }
-}
-
-fun numStonesGeneratedCached(stone: Long, blinksLeft: Int): Long {
-    val cached = cache[Pair(stone, blinksLeft)]
-    if (cached != null) return cached
-
-    val ans = numStonesGenerated(stone, blinksLeft)
-    cache[Pair(stone, blinksLeft)] = ans
-    return ans
-}
-
 fun main() {
     val exampleLines = readInput("2024", "day11-example")
     val input = readInput("2024", "day11")
 
+    fun blink(stone: Long): List<Long> {
+        if (stone == 0L) return listOf(1L)
+        val str = stone.toString()
+        if (str.length % 2 == 0) {
+            return listOf(
+                str.substring(0, str.length / 2).toLong(),
+                str.substring(str.length / 2).toLong()
+            )
+        }
+        return listOf((stone * 2024L))
+    }
+
+    val cache = mutableMapOf<Pair<Long, Int>, Long>()
+
+    fun numStonesGenerated(stone: Long, blinksLeft: Int): Long {
+        return cache.getOrPut(Pair(stone, blinksLeft)) {
+            if (blinksLeft == 0) return 1
+            return blink(stone).sumOf { numStonesGenerated(it, blinksLeft - 1) }
+        }
+    }
+
     fun getStonesGenerated(input: String, blinks: Int): Long {
         val startStones = input.split(" ").map { it.toLong() }
-        return startStones.sumOf { numStonesGeneratedCached(it, blinks) }
+        return startStones.sumOf { numStonesGenerated(it, blinks) }
     }
 
-    fun part1(input: String): Long {
-        return getStonesGenerated(input, 25)
-    }
-
-    fun part2(input: String): Long {
-        return getStonesGenerated(input, 75)
-    }
+    fun part1(input: String): Long = getStonesGenerated(input, 25)
+    fun part2(input: String): Long = getStonesGenerated(input, 75)
 
     val part1Example = part1(exampleLines)
 
